87859db30286b33ea88c2bffa9548b9c083126be
LeviathanST <phamquochung170505@gmail.com>
2025-03-08 15:37:55 +0700

    update: store names and ages in memory

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include "stdio.h"
 
 int main() {
-  printf("Hello, TinySQL!");
+  char names[3][20] = {"Alice", "Bob"};
+  int ages[3] = {30, 31};
+
+  char format[] = "%s - %d\n"; // name - age
+
+  printf(format, names[0], ages[0]);
+  printf(format, names[1], ages[1]);
   return 0;
 }
