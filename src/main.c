287d70f5388f5bf8e5b838a0901166832c97277a
LeviathanST <phamquochung170505@gmail.com>
2025-03-11 12:06:27 +0700

    update(main): make data lives by user input

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
-#include "stdio.h"
+#include <stddef.h>
+#include <stdio.h>
 
 int main() {
   char names[3][20] = {"Alice", "Bob"};
   int ages[3] = {30, 31};
 
   char format[] = "%s - %d\n"; // name - age
+  printf("Type a name: \n");
+  scanf("%s", names[2]);
+  printf("Type a age: \n");
+  scanf("%d", &ages[2]);
 
-  printf(format, names[0], ages[0]);
-  printf(format, names[1], ages[1]);
+  printf(format, names[2], ages[2]);
   return 0;
 }
