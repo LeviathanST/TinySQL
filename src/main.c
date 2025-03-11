80f9f1c89f42ba233d052983a306bd3eca557bbd
LeviathanST <phamquochung170505@gmail.com>
2025-03-11 12:24:13 +0700

    update(main): more information slot

--- src/main.c
+++ src/main.c
@@ -1,16 +1,32 @@
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 
 int main() {
-  char names[3][20] = {"Alice", "Bob"};
-  int ages[3] = {30, 31};
+  char names[100][20] = {"Alice", "Bob"};
+  int ages[100] = {30, 31};
+  int curr_idx = 2;
 
   char format[] = "%s - %d\n"; // name - age
-  printf("Type a name: \n");
-  scanf("%s", names[2]);
-  printf("Type a age: \n");
-  scanf("%d", &ages[2]);
+  bool out = false;
 
-  printf(format, names[2], ages[2]);
+  size_t names_length = sizeof(names) / sizeof(names[0]);
+  while (!out) {
+    if (curr_idx > names_length) {
+      printf("Maximum data amount");
+    }
+    printf("Type a name: \n");
+    scanf("%s", names[curr_idx]);
+    printf("Type a age: \n");
+    scanf("%d", &ages[curr_idx]);
+    printf("The crew: \n");
+
+    for (int i = 0; i <= curr_idx; i++) {
+      printf(format, names[i], ages[i]);
+    }
+
+    curr_idx++;
+  }
+
   return 0;
 }
