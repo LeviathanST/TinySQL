954d3847175e5cc0b0a3ac40270f6db9fc7fca88
LeviathanST <phamquochung170505@gmail.com>
2025-03-11 18:16:25 +0700

    update(main): more meaning by using struct

--- src/main.c
+++ src/main.c
@@ -2,27 +2,34 @@
 #include <stddef.h>
 #include <stdio.h>
 
+struct Person {
+  char name[20];
+  int age;
+};
+
 int main() {
-  char names[100][20] = {"Alice", "Bob"};
-  int ages[100] = {30, 31};
+  struct Person people[100] = {
+      {"Alice", 30},
+      {"Bob", 31},
+  };
   int curr_idx = 2;
 
   char format[] = "%s - %d\n"; // name - age
   bool out = false;
 
-  size_t names_length = sizeof(names) / sizeof(names[0]);
   while (!out) {
-    if (curr_idx > names_length) {
-      printf("Maximum data amount");
-    }
-    printf("Type a name: \n");
-    scanf("%s", names[curr_idx]);
-    printf("Type a age: \n");
-    scanf("%d", &ages[curr_idx]);
-    printf("The crew: \n");
+    struct Person p;
+
+    printf("Type name: ");
+    scanf("%s", p.name);
+
+    printf("Type age: ");
+    scanf("%d", &p.age);
+
+    people[curr_idx] = p;
 
     for (int i = 0; i <= curr_idx; i++) {
-      printf(format, names[i], ages[i]);
+      printf(format, people[i].name, people[i].age);
     }
 
     curr_idx++;
