c7d33f22e9eed69180365ab82545eafc84276bf9
LeviathanST <phamquochung170505@gmail.com>
2025-03-12 18:26:30 +0700

    update(main): enhance UI

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 typedef struct {
   char name[20];
@@ -10,35 +8,22 @@ typedef struct {
 } Person;
 
 int main() {
-  int capacity = 4;
-  int total = 3;
-  Person *p = malloc(sizeof(Person) * capacity);
-
-  char format[] = "%s - %d\n"; // name - age
-
-  strcpy(p[0].name, "Alice");
-  p[0].age = 30;
-
-  strcpy(p[1].name, "Bob");
-  p[1].age = 31;
-
   while (true) {
-    if (total > capacity) {
-      capacity *= 2;
-      p = realloc(p, sizeof(Person) * capacity);
+    printf("1: Add\n");
+    printf("2: Get All\n");
+
+    int choice;
+    scanf("%d", &choice);
+    switch (choice) {
+    case 1:
+      // Function to write a soul
+      break;
+    case 2:
+      // Function to read all souls
+      break;
+    default:
+      break;
     }
-
-    printf("Type name:\n");
-    scanf("%s", p[total - 1].name);
-
-    printf("Type age:\n");
-    scanf("%d", &p[total - 1].age);
-
-    for (int i = 0; i < total; i++) {
-      printf(format, p[i].name, p[i].age);
-    }
-    total++;
   }
-  free(p);
   return 0;
 }
