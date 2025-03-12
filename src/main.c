3d99d5db0766c48d6e2b3180c404be08b8d08421
LeviathanST <phamquochung170505@gmail.com>
2025-03-12 18:46:51 +0700

    feat(main): init data file if not exist

--- src/main.c
+++ src/main.c
@@ -1,13 +1,38 @@
+#include <errno.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PATH "/path/to/data/file"
+#define DEFAULT_TOTAL 0
 
 typedef struct {
   char name[20];
   int age;
 } Person;
 
+// NOTE: Init data file if not exists
+void init() {
+  int is_exists = access(PATH, F_OK);
+  if (is_exists == -1) {
+    FILE *p_file = fopen(PATH, "wb+");
+    if (p_file == NULL) {
+      printf("Cannot init data file for souls: %s", strerror(errno));
+      return;
+    }
+    int default_total = DEFAULT_TOTAL;
+    fwrite(&default_total, 2, 1, p_file);
+    fclose(p_file);
+    return;
+  }
+
+  printf("Data file is exists!");
+}
+
 int main() {
+  init();
   while (true) {
     printf("1: Add\n");
     printf("2: Get All\n");
