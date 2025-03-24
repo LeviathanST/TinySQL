004e78b29c554fa4f701e9f0dc6fe2e8edfd3d20
LeviathanST <phamquochung170505@gmail.com>
2025-03-24 23:17:32 +0700

    refactor(soul/find_a): indexing for faster access

--- src/soul.c
+++ src/soul.c
@@ -10,28 +10,55 @@
 int soul_init(const char *path) {
   int is_exists = access(path, F_OK);
   if (is_exists == -1) {
-    FILE *p_file = fopen(path, "wb+");
+    FILE *p_file = fopen(path, "ab");
     if (p_file == NULL) {
       printf("Cannot init data file for souls: %s\n", strerror(errno));
       return 0;
     }
-    short default_total = DEFAULT_TOTAL;
-    fwrite(&default_total, 2, 1, p_file);
+
+    /* 2 bytes - the number of current total entities
+     * 512 bytes - the index area
+     * 512 bytes - the data area
+     * */
+    unsigned char *buffer = calloc(1, 1026);
+    fwrite(buffer, 514, 1, p_file);
+
     fclose(p_file);
   }
   return 1;
 }
 
-void soul_increase_curr_total(const char *path) {
+/* This function will write entity's index into the index area then increasing
+ * the number of current total entites.
+ **/
+void soul_write_index_and_increase_curr_total(const char *path, char key[8]) {
   FILE *p_file = fopen(path, "rb+");
+  Index i;
   if (!p_file) {
     printf("Cannot open souls data file: %s", strerror(errno));
     return;
   }
-
-  rewind(p_file);
+  // Read and increase the current total in data file
   short curr_total;
   fread(&curr_total, 2, 1, p_file);
+  if (curr_total * 12 > 512) {
+    printf("The index area is full! %u", curr_total);
+    return;
+  }
+
+  strncpy(i.key, key, 8);
+  i.offset = (unsigned int)(curr_total * sizeof(Person));
+
+  // Go to n-th index in the index area from the beginning
+  // 0 -> 1   - the number of current total (skip)
+  // n -> (n + 12) - 1 (n = the previous ended value + 1)
+  // 2 -> 13  - the first entity
+  // 14 -> 25 - the second entity
+  fseek(p_file, 2 + curr_total * (sizeof(char *) + sizeof(unsigned int)),
+        SEEK_SET);
+  fwrite(i.key, sizeof(char *), 1, p_file);
+  fwrite(&i.offset, sizeof(unsigned int), 1, p_file);
+
   curr_total++;
   rewind(p_file);
   fwrite(&curr_total, 2, 1, p_file);
@@ -40,16 +67,16 @@ void soul_increase_curr_total(const char *path) {
 }
 
 int soul_write_a(const char *path, Person *p) {
+  soul_write_index_and_increase_curr_total(path, p->name);
   FILE *p_file = fopen(path, "ab");
 
   if (!p_file) {
     printf("Cannot open souls data file: %s\n", strerror(errno));
     return 0;
   }
-  fwrite(p, sizeof(Person), 1, p_file);
 
+  fwrite(p, sizeof(Person), 1, p_file);
   fclose(p_file);
-  soul_increase_curr_total(path);
   return 1;
 }
 
@@ -67,7 +94,7 @@ ResultSet *soul_read_all(const char *path) {
   fread(&curr_total, 2, 1, p_file);
 
   Person *buffer = calloc(curr_total, sizeof(Person));
-  fseek(p_file, 2, SEEK_SET);
+  fseek(p_file, 514, SEEK_SET);
   fread(buffer, sizeof(Person), curr_total, p_file);
 
   rs->count = curr_total;
@@ -77,9 +104,7 @@ ResultSet *soul_read_all(const char *path) {
   return rs;
 }
 
-// TODO: Enhance performance
-// - Using index
-Person *soul_find_a_with_name(const char *path, char *name) {
+Person *soul_find_a_with_name(const char *path, const char *name) {
   FILE *p_file = fopen(path, "rb");
   if (!p_file) {
     printf("Cannot open souls data file: %s", strerror(errno));
@@ -91,25 +116,25 @@ Person *soul_find_a_with_name(const char *path, char *name) {
   fread(&curr_total, 2, 1, p_file);
 
   ht *p_ht = ht_create(curr_total);
-  Person *buffer = calloc(curr_total, sizeof(Person));
+  Index *index = calloc(12, curr_total);
   fseek(p_file, 2, SEEK_SET);
-  fread(buffer, sizeof(Person), curr_total, p_file);
+  fread(index, sizeof(Index), curr_total, p_file);
 
   for (int i = 0; i < curr_total; i++) {
-    if (!ht_insert(p_ht, buffer[i].name, &buffer[i])) {
-      printf("Something went wrong when insert in hash table!");
-      return 0;
-    }
+    ht_insert(p_ht, index[i].key, &index[i].offset);
   }
 
-  Person *result = ht_get(p_ht, name);
-  if (!result) {
+  unsigned int *offset = ht_get(p_ht, name);
+  if (!offset) {
     printf("Not Found!\n");
     return 0;
   }
+  Person *p = malloc(sizeof(Person));
+  fseek(p_file, 514 + *offset, SEEK_SET);
+  fread(p, sizeof(Person), 1, p_file);
 
+  free(index);
   fclose(p_file);
-  free(buffer);
   ht_clear(p_ht);
-  return result;
+  return p;
 }
--- src/soul.h
+++ src/soul.h
@@ -1,20 +1,21 @@
 #ifndef _SOUL_H
 #define _SOUL_H
 
-#define DEFAULT_TOTAL 0
 typedef struct Index Index;
-struct Index {
-  const char *key;
+typedef struct Index {
+  char key[8];
   unsigned int offset;
-};
+} Index;
+
 typedef struct ResultSet ResultSet;
 struct ResultSet {
   unsigned int count;
   void *p_rows; // Rows Array
 };
+
 typedef struct Person Person;
 struct Person {
-  char name[256]; // Unique
+  char name[20]; // Unique
   unsigned int age;
 };
 
@@ -31,6 +32,6 @@ int soul_write_a(const char *path, Person *p);
 ResultSet *soul_read_all(const char *path);
 
 /// Return 0 if not exists.
-Person *soul_find_a_with_name(const char *path, char *name);
+Person *soul_find_a_with_name(const char *path, const char *name);
 
 #endif
