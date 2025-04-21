9a58a34ea2086bbc1852cdc265dec24acc294534
LeviathanST <phamquochung170505@gmail.com>
2025-04-21 20:21:56 +0700

    add: more controls with ui

--- src/main.zig
+++ src/main.zig
@@ -1,26 +1,55 @@
 const std = @import("std");
+const Writer = std.io.Writer;
+const Reader = std.io.Reader;
 
 const MAX_LIST_LENGTH: usize = 2;
+var current_idx: usize = 0;
 
 const Soul = struct {
     name: []const u8,
     age: u8,
 };
 
-fn display_souls(list: *[MAX_LIST_LENGTH]Soul) anyerror!void {
-    const stdout = std.io.getStdOut().writer();
-
+fn display_souls(
+    stdout: anytype,
+    list: *[MAX_LIST_LENGTH]Soul,
+) anyerror!void {
     try stdout.writeAll("Result:\n");
     try stdout.writeAll("----\n");
-    for (list) |soul| {
+    if (current_idx <= 0) {
+        try stdout.writeAll("The storage is empty!\n");
+    }
+    for (list[0..current_idx]) |soul| {
         try stdout.print("Name {s}\n", .{soul.name});
         try stdout.print("Age {d}\n", .{soul.age});
         try stdout.writeAll("----\n");
     }
 }
-fn write_soul(list: *[MAX_LIST_LENGTH]Soul, idx: usize, name: []u8, age: u8) void {
+fn write_soul(
+    stdin: anytype,
+    stdout: anytype,
+    allocator: std.mem.Allocator,
+    list: *[MAX_LIST_LENGTH]Soul,
+    idx: usize,
+) anyerror!void {
+    var name_buffer: [100]u8 = undefined;
+    var age_buffer: [10]u8 = undefined;
+
+    // readUntilDelimiter is deprecated since 0.11.0
+    // but is still used and I don't know why.
+    // I wanna use it and find out why.
+    // Issue: https://github.com/ziglang/zig/issues/15528
+    try stdout.print("Name:\n", .{});
+    var fbs = std.io.fixedBufferStream(&name_buffer);
+    try stdin.streamUntilDelimiter(fbs.writer(), '\n', fbs.buffer.len);
+    var name_copy = try allocator.alloc(u8, fbs.getWritten().len);
+    @memcpy(name_copy[0..], fbs.getWritten());
+
+    try stdout.print("Age:\n", .{});
+    const age_input = try stdin.readUntilDelimiter(&age_buffer, '\n');
+    const age = try std.fmt.parseInt(u8, age_input, 10);
     const soul = Soul{
-        .name = name,
+        .name = name_copy,
         .age = age,
     };
 
@@ -29,7 +58,6 @@ fn write_soul(list: *[MAX_LIST_LENGTH]Soul, idx: usize, name: []u8, age: u8) voi
 
 pub fn main() !void {
     var souls: [MAX_LIST_LENGTH]Soul = undefined;
-    var current_idx: usize = 0;
 
     // Use ArenaAllocator to allocate mutiple times and only once free.
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -39,21 +67,36 @@ pub fn main() !void {
     const stdin = std.io.getStdIn().reader();
     const stdout = std.io.getStdOut().writer();
 
-    var name_buffer: [100]u8 = undefined;
-    var age_buffer: [100]u8 = undefined;
+    var choice_buf: [2]u8 = undefined;
 
-    while (current_idx < souls.len) : (current_idx += 1) {
-        try stdout.print("Name:\n", .{});
-        const name = try stdin.readUntilDelimiter(&name_buffer, '\n');
-        const name_copy = try allocator.alloc(u8, name.len);
-        @memcpy(name_copy, name);
-
-        try stdout.print("Age:\n", .{});
-        const age_input = try stdin.readUntilDelimiter(&age_buffer, '\n');
-        const age = try std.fmt.parseInt(u8, age_input, 10);
-
-        write_soul(&souls, current_idx, name_copy, age);
+    while (true) {
+        const choice_input = try stdin.readUntilDelimiter(&choice_buf, '\n');
+        const choice = try std.fmt.parseInt(u8, choice_input, 10);
+        switch (choice) {
+            1 => {
+                if (current_idx < souls.len) {
+                    try write_soul(
+                        stdin,
+                        stdout,
+                        allocator,
+                        &souls,
+                        current_idx,
+                    );
+                    current_idx += 1;
+                    continue;
+                } else {
+                    try stdout.writeAll("The storage is full!");
+                    continue;
+                }
+            },
+            2 => {
+                try display_souls(stdout, &souls);
+                continue;
+            },
+            else => {
+                try stdout.writeAll("Your choice not found!");
+                continue;
+            },
+        }
     }
-
-    try display_souls(&souls);
 }
