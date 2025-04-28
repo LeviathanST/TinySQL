a533e8018bb068fe63764e4b46a9aedc6659b5d8
LeviathanST <phamquochung170505@gmail.com>
2025-04-29 03:24:57 +0700

    (ui/soul): update functions and add menuPrompt

--- src/ui/soul.zig
+++ src/ui/soul.zig
@@ -15,35 +15,54 @@ ctx: TinySQLContext,
 pub fn init(comptime ctx: TinySQLContext) Self {
     return Self{ .ctx = ctx };
 }
+// Return user choice number
+pub fn menuPrompt(self: Self) !u8 {
+    const stdout = self.ctx.stdout;
+    try stdout.writeAll("1> Insert new\n");
+    try stdout.writeAll("2> Display all\n");
+    try stdout.writeAll("3> Find\n");
+    try stdout.writeAll("4> Delete\n");
+    var choice_buf: [2]u8 = undefined;
+    const choice_input = try self.ctx.stdin.readUntilDelimiter(&choice_buf, '\n');
+    return try std.fmt.parseInt(u8, choice_input, 10);
+}
 
 pub fn displayAll(self: Self, list: SoulList) !void {
-    const ctx = self.ctx;
+    const stdout = self.ctx.stdout;
 
-    try ctx.stdout.print("{s}\n", .{"-" ** 45});
-    try ctx.stdout.print("Name{s}|| Age\n", .{" " ** 32});
-    try ctx.stdout.print("{s}\n", .{"-" ** 45});
-    for (list.items[0..list.current_total]) |soul| {
-        try ctx.stdout.print("{s}{s}|| {d}\n", .{ soul.name, " " ** 32, soul.age });
-    }
-    try ctx.stdout.print("{s}\n", .{"-" ** 45});
-    try ctx.stdout.print("Total: {}\n", .{list.current_total});
-    try ctx.stdout.print("{s}\n", .{"-" ** 45});
-}
-pub fn displayA(self: Self, soul: ?Soul) !void {
-    const ctx = self.ctx;
-
-    try ctx.stdout.print("{s}\n", .{"-" ** 45});
-    try ctx.stdout.print("Name{s}|| Age\n", .{" " ** 32});
-    try ctx.stdout.print("{s}\n", .{"-" ** 45});
-    if (soul) |value| {
-        try ctx.stdout.print("{s}{s}|| {d}\n", .{ value.name, " " ** 32, value.age });
+    try stdout.print("{s}\n", .{"-" ** 45});
+    try stdout.print("Name{s}|| Age\n", .{" " ** 32});
+    try stdout.print("{s}\n", .{"-" ** 45});
+    if (list.items.len <= 0) {
+        try stdout.writeAll("Empty\n");
     } else {
-        try ctx.stdout.writeAll("Not Found\n");
+        for (list.items[0..list.current_total]) |soul| {
+            try stdout.print("{s}{s}|| {d}\n", .{ soul.name, " " ** 32, soul.age });
+        }
     }
-    try ctx.stdout.print("{s}\n", .{"-" ** 45});
+
+    try stdout.print("{s}\n", .{"-" ** 45});
+    try stdout.print("Total: {}\n", .{list.current_total});
+    try stdout.print("{s}\n", .{"-" ** 45});
 }
+
+pub fn displayA(self: Self, soul: ?Soul) !void {
+    const stdout = self.ctx.stdout;
+
+    try stdout.print("{s}\n", .{"-" ** 45});
+    try stdout.print("Name{s}|| Age\n", .{" " ** 32});
+    try stdout.print("{s}\n", .{"-" ** 45});
+    if (soul) |value| {
+        try stdout.print("{s}{s}|| {d}\n", .{ value.name, " " ** 32, value.age });
+    } else {
+        try stdout.writeAll("Not Found\n");
+    }
+    try stdout.print("{s}\n", .{"-" ** 45});
+}
+
 pub fn writePrompt(self: Self) !Soul {
-    const ctx = self.ctx;
+    const stdout = self.ctx.stdout;
+    const stdin = self.ctx.stdin;
     var name_buffer: [100]u8 = undefined;
     var age_buffer: [3]u8 = undefined;
 
@@ -51,13 +70,13 @@ pub fn writePrompt(self: Self) !Soul {
     // but is still used and I don't know why.
     // I wanna use it and find out why.
     // Issue: https://github.com/ziglang/zig/issues/15528
-    try ctx.stdout.print("Name: ", .{});
+    try stdout.print("Name: ", .{});
     var fbs = std.io.fixedBufferStream(name_buffer[0..]);
-    try ctx.stdin.streamUntilDelimiter(fbs.writer(), '\n', fbs.buffer.len);
+    try stdin.streamUntilDelimiter(fbs.writer(), '\n', fbs.buffer.len);
     var name_input = fbs.getWritten();
 
-    try ctx.stdout.print("Age: ", .{});
-    const age_input = try ctx.stdin.readUntilDelimiter(age_buffer[0..], '\n');
+    try stdout.print("Age: ", .{});
+    const age_input = try stdin.readUntilDelimiter(age_buffer[0..], '\n');
     const age = try std.fmt.parseInt(u8, age_input, 10);
     return try Soul.fromUserInput(name_input[0..], age);
 }
