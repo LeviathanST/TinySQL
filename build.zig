174d59b31ed1a3a22ebc9d526933c0b9e5f7d6b7
LeviathanST <phamquochung170505@gmail.com>
2025-04-29 03:26:46 +0700

    update(build): run test after build

--- build.zig
+++ build.zig
@@ -3,13 +3,13 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
     const exe = b.addExecutable(.{
         .name = "TinySQL",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-
     b.installArtifact(exe);
     const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run the application");
@@ -20,6 +20,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    const run_unit_test = b.addRunArtifact(main_tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&run_unit_test.step);
 }
