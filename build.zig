083f0b72d4e4169eb6cbb8e591ec954b8eb28579
LeviathanST <phamquochung170505@gmail.com>
2025-05-05 23:44:34 +0700

    update(build): build bench exe

--- build.zig
+++ build.zig
@@ -15,6 +15,16 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_exe.step);
 
+    const bench_exe = b.addExecutable(.{
+        .name = "Bench",
+        .root_source_file = b.path("src/benchmark.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(bench_exe);
+    const run_bench_step = b.step("bench", "Build the benchmark");
+    run_bench_step.dependOn(&bench_exe.step);
+
     const test_cmd = b.addSystemCommand(&.{ "sh", "-c", "zig test src/main.zig 2>&1 | cat" });
     const test_step = b.step("test", "Run unit tests"); // https://github.com/ziglang/zig/issues/10203
     test_step.dependOn(&test_cmd.step);
